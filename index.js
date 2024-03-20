a6d87e884542eebb3f7086ee710e94c48d427a65
ngure808 <mwangingure808@gmail.com>
2024-03-20 14:45:02 +0300

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(lists){
+    return lists = ['Antonia', 'Nuru']
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const returnLastTwoDrivers = function(last){
+    return last = ['Amari', 'Mo']
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'])
+
+const selectingDrivers = ([returnFirstTwoDrivers, returnLastTwoDrivers])
+
+const createFareMultiplier = function(integer){
+    return (function fareMultiplier(num){
+        return integer * num
+    })
+}
+
+const fareDoubler = function doubles(createFareMultiplier){
+    return createFareMultiplier * 2
+}
+
+const fareTripler = function triples(createFareMultiplier){
+    return createFareMultiplier * 3
+}
+
+const selectDifferentDrivers = function (drivers, selectingDrivers){
+    drivers['Antonia','Nuru','Amari','Mo']
+    return selectingDrivers()
+}
